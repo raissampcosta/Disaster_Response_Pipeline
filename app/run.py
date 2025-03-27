f0f5b5cd14fa40128f2bb8ad1e82704adc6a0e50
Raissa Costa <raissa.mpcosta@outlook.com>
2025-03-27 00:50:25 -0300

    Update master.html to correctly render classification output

--- app/run.py
+++ app/run.py
@@ -1,6 +1,7 @@
 import json
 import plotly
 import pandas as pd
+import joblib
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -8,7 +9,6 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
 
